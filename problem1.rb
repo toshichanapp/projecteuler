b92cd8f561a9808a4b8101fa38dea7c35a89340e
toshichanapp <toshichanapp@gmail.com>
2019-12-08 17:55:41 +0900

    fix problem1

--- problem1.rb
+++ problem1.rb
@@ -1,10 +1,1 @@
-N = 1000
-third = []
-fifth = []
-
-N.times do |i|
-  third << i if i % 3 == 0
-  fifth << i if i % 5 == 0
-end
-
-p third.concat(fifth).uniq.reduce(:+)
+(1..999).select{|num| num % 3 == 0 || num % 5 == 0}.sum
