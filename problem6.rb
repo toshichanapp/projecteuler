aa453ca924d096a1b16dea4da63c20dcbfd533ba
ToshikiFujiyoshi <36914210+toshichanapp@users.noreply.github.com>
2021-04-04 13:38:18 +0900

    Update problem6.rb

--- problem6.rb
+++ problem6.rb
@@ -1,4 +1,4 @@
 range = (1..100)
 sum_of_squares = range.reduce(0){ |result, num| result + num ** 2 }
-sum_squared = range.reduce(:+) ** 2
+sum_squared = range.sum ** 2
 puts sum_squared - sum_of_squares
