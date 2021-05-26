c0f2b3a243c9b9f5faf11b75e6b62f230a656686
toshichanapp <toshichanapp@gmail.com>
2021-05-26 16:15:29 +0900

    17

---
+++ 11-20/problem17.rb
@@ -0,0 +1,31 @@
+# TODO: programming!!!
+
+# one two three four five six seven eight nine
+# [3, 3, 5, 4, 4, 3, 5, 5, 4]
+# 36
+
+# ten
+# 3
+
+# eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen
+# 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8 = 67
+
+# twenty thirty forty fifty sixty seventy eighty ninety
+# [6, 6, 5, 5, 5, 7, 6, 6].map{|n| n * 10 + 36}
+# [96, 96, 86, 86, 86, 106, 96, 96].sum
+# 748
+
+# one..ninety nine
+# [36, 3, 67, 748].sum
+# 854
+
+# hundred
+# one * 100 + hundred * 100 + and * 99 + 854
+# [3, 3, 5, 4, 4, 3, 5, 5, 4].map{ |n| n * 100 + 7 * 100 + 3 * 99 + 854 }.sum
+# 20259
+
+# thousand
+# one thousand -> 11
+
+# 20259 + 854 + 11
+# 21124
