215f7d3732ebde9aff4661cef7a1215881cd1c68
toshichanapp <toshichanapp@gmail.com>
2021-06-05 15:06:05 +0900

    fix 32

--- 31-40/problem32.rb
+++ 31-40/problem32.rb
@@ -1,4 +1,22 @@
-# FIXME: wrong answer!!!
+=begin
+積 1
+[1, 7], ・・・
+
+積 2
+[1, 6], [2, 5], [3, 4]
+
+積 3
+[1, 5], [2, 4], [3, 3]
+
+積 4
+[1, 4], [2, 3]
+
+積 5
+[1, 3], [2, 2]
+
+積がパンデジタルになりえるのは
+積 4 [1, 4], [2, 3]のみ
+=end
 
 require 'set'
 @nums = (1..9).to_a
@@ -9,8 +27,9 @@ def calc_product(m, n)
     multiplier_arr = @nums - multiplicand_arr
     multiplier_arr.permutation(n).each do |multiplier|
       product = multiplier.join.to_i * multiplicand_arr.join.to_i
+      next unless product.to_s.length == 4
       set = Set.new(product.to_s.chars.map(&:to_i)) + Set.new(multiplier) + Set.new(multiplicand_arr)
-      if set.length == @nums.length && product <= 9999
+      if !set.include?(0) && set.length == @nums.length
         @products << product
       end
     end
